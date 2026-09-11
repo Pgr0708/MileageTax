95bcf4c5dffd31832311b3cc64a2d36401f52ae0
Pgr0708 <parthgohil0708@gmail.com>
2026-09-11 15:29:06 +0530

    Initial Commit

---
+++ MileageTax/MileageTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTaxApp.swift
+//  MileageTax
+//
+//  Created by Minaxi on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MileageTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
